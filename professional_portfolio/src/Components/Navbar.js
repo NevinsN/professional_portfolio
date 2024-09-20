34994ec7a5834178c4e51eb519da5ed38cab071e
Nicholas Nevins <47232970+NevinsN@users.noreply.github.com>
2024-09-20 12:46:00 -0600

    Update Navbar.js
    Stand in navbar component

---
+++ professional_portfolio/src/Components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+    
+        <nav className="navbar">
+            <p>Navbar</p>
+        </nav>
+    );
+};
+
+export default Navbar;
