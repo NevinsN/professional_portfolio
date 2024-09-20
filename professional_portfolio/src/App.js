411add6467cdd258672e4ba55f667416a48b33ba
Nicholas Nevins <47232970+NevinsN@users.noreply.github.com>
2024-09-20 08:34:10 -0600

    Update App.js
    Elevator pitch added

--- professional_portfolio/src/App.js
+++ professional_portfolio/src/App.js
@@ -12,6 +12,13 @@ function App() {
         <h1>
           Nicholas Nevins
         </h1>
+        <p>A results-driven software developer with a strong technical skillset. 
+            Excels at problem solving, learning new skills quickly, and possesses 
+            a strong background in customer service and team-building. Consistently 
+            recognized for abilities to train and teach others as a functional 
+            member of a team. Strong written and verbal interpersonal communication 
+            skills. Team player with the ability to work independently.
+        </p>
       </header>
     </div>
   );
