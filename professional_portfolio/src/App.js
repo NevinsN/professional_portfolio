c2bba23c1d7f1d497b0331a4f4f721fcb96e560a
Nicholas Nevins <47232970+NevinsN@users.noreply.github.com>
2024-10-01 10:24:54 -0600

    Update App.js

--- professional_portfolio/src/App.js
+++ professional_portfolio/src/App.js
@@ -5,6 +5,7 @@ import {
     BrowserRouter as Router,
     Routes,
     Route,
+    Switch
 } from "react-router-dom";
 import { Navigate } from 'react-router-dom';
 import Home from "./pages/home";
