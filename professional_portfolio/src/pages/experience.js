da35a83de5c6a97cf6aef04b8637bbd619ac0e79
Nicholas Nevins <47232970+NevinsN@users.noreply.github.com>
2024-10-14 20:05:49 -0600

    ExperienceAdded

--- professional_portfolio/src/pages/experience.js
+++ professional_portfolio/src/pages/experience.js
@@ -12,6 +12,26 @@ const Experience = () => {
             }}
         >
             <h1>Experience</h1>
+            <p>
+                <h2>Field Sales Specialist, Talking Rain Beverage Company, Idaho Falls, ID  	 	          2023 – Present</h2> 
+                <ul>
+                    <li>Represented the beverage brand in Eastern Idaho, effectively interfacing with store leaders, distributor partners, and company personnel to establish and maintain exceptional relationships, contributing to the growth of regional sales.</li>
+                    <li>Build and foster relationships strategically to leverage into increased sales and secure prominent display features, resulting in a substantial boost in overall sales for the region.</li>  
+                    <li>Foster a collaborative and results-driven team environment by utilizing the right tools to complete necessary tasks in a timely manner.  </li>
+                    <li>Establish and achieve goals with a results-oriented mindset, contributing to the overall success of the sales team and the beverage brand in Eastern Idaho.</li>  
+                    <li>Communicate effectively with diverse stakeholders, ensuring clear and concise information exchange to facilitate collaborative efforts and achieve common goals.</li>
+                </ul>
+            </p>
+            <p>
+                <h2>Food Receiver, Fred Meyer, Idaho Falls, ID</h2>         	 	 	 	 	 	          2010 – 2023 
+                <ul>
+                    <li>Mastered company technology and processes, serving as an expert to coworkers when they encountered challenges with the company's systems, increasing the team's overall proficiency.</li>
+                    <li>Established and built relationships with vendors from various companies and backgrounds, fostering positive and collaborative partnerships.</li>  
+                    <li>Demonstrate strong organizational and time management skills by managing individual priorities to accomplish all daily tasks.</li>  
+                    <li>Created a file storage system to comply with company and governmental regulations, ensuring adherence to compliance standards and efficient data management.</li>  
+                    <li>Proactively addressed challenges and solved problems related to product receiving, ensuring a smooth flow of operations and maintaining a high level of accuracy.</li>
+                </ul>
+            </p>
         </div>
         </header>
     );
