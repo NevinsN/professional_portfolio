5c692caae31a4e0cc4f7509b897207ce2271dea4
Nicholas Nevins <47232970+NevinsN@users.noreply.github.com>
2024-10-25 00:38:06 -0600

    ImageAndFormat
    Added headshot and formatting. Cleaned up structure some.

--- professional_portfolio/src/pages/home.js
+++ professional_portfolio/src/pages/home.js
@@ -1,28 +1,24 @@
 import React from 'react';
+import Headshot from "../Images/Headshot.jpg";
 
 const Home = () => {
   return (
       <header className="App-header">      
-        <h1>
-              Nicholas Nevins
+        <h1 className="App-header_title">
+          Nicholas Nevins - Software Developer
         </h1>
-        <div style={{
-                display: "flex",
-                justifyContent: "centre",
-                alignContent: "center",
-                height: "100vh",
-            }}>
+        <div className="content">
+          <img src={Headshot} alt="Nick Nevins Headshot" style={{width:"200px", height:"238px",}}/>
           <p style={{
-            fontSize: "17px",
-            marginLeft: "25%",
-            width: "50%",
+              fontSize: "17px",
+              width: "50%",
           }}>
-            A results-driven software developer with a strong technical skillset. 
-            Excels at problem solving, learning new skills quickly, and possesses 
-            a strong background in customer service and team-building. Consistently 
-            recognized for abilities to train and teach others as a functional 
-            member of a team. Strong written and verbal interpersonal communication 
-            skills. Team player with the ability to work independently.
+              A results-driven software developer with a strong technical skillset. 
+              Excels at problem solving, learning new skills quickly, and possesses 
+              a strong background in customer service and team-building. Consistently 
+              recognized for abilities to train and teach others as a functional 
+              member of a team. Strong written and verbal interpersonal communication 
+              skills. Team player with the ability to work independently.
           </p>
         </div>
       </header>
---
+++ professional_portfolio/src/pages/projects/capstone.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Capstone = () => {
+    return (
+        <header className="App-header">
+        <div
+            style={{
+                display: "flex",
+                justifyContent: "centre",
+                alignItems: "centre",
+                height: "100vh",
+            }}
+        >
+            <h1>Capstone Project</h1>
+        </div>
+        </header>
+    );
+};
+
+export default Capstone;
