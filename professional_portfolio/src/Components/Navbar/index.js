2f941a9fe79de6ccfc75452bd9cd7aa565a48ac6
Nicholas Nevins <47232970+NevinsN@users.noreply.github.com>
2025-10-09 20:45:13 -0600

    Error Fix Hamburger Menu
    Fixed an error. Pushing to check

--- professional_portfolio/src/Components/Navbar/index.js
+++ professional_portfolio/src/Components/Navbar/index.js
@@ -1,10 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import {
     Nav,
     NavLink,
     Bars,
-    NavMenu,
-    useState
+    NavMenu
 } from "./NavbarElements";
 
 const Navbar = () => {
