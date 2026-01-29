a7675f7533b11544bc64de3f46adddd2d7036fe4
Nicholas Nevins <47232970+NevinsN@users.noreply.github.com>
2026-01-29 10:14:06 -0700

    added Footer to app.js

--- professional_portfolio/src/App.js
+++ professional_portfolio/src/App.js
@@ -7,6 +7,7 @@ import {
     Route,
 } from "react-router-dom";
 import { Navigate } from 'react-router-dom';
+import Footer from "./Components/Footer";
 import Home from "./pages/home";
 import Experience from "./pages/experience";
 import Projects from "./pages/projects";
@@ -43,6 +44,7 @@ function App() {
                 <Route path="/skills" element={<Skills theme={theme} />} />
                 <Route path="/" element={<Navigate replace to="/home" />} />
             </Routes>
+            <Footer />
     </Router>
     );
 }
