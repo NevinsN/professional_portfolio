f5d2fc611bac9b4c7e92e7b549040150f78159e1
Nicholas Nevins <47232970+NevinsN@users.noreply.github.com>
2026-02-05 12:53:46 -0700

    Update App.js

--- professional_portfolio/src/App.js
+++ professional_portfolio/src/App.js
@@ -20,7 +20,7 @@ function App() {
       <div className={`App ${theme}`}>
         <nav className="Nav">
           <div className="NavMenu">
-            <Link color="inherit" to="/">Home</Link>
+            <Link to="/" className="nav-link">Home</Link>
             <Link to="/projects">Projects</Link>
             <Link to="/experience">Experience</Link>
             <Link to="/skills">Skills</Link>
